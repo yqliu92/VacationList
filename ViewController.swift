76f4ac6500c606c8f6a716ef64cea79c350cae27
yqliu92 <yliu252@hawk.iit.edu>
2016-08-03 01:56:31 -0500

    Program

---
+++ ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VacationList
+//
+//  Created by liuyunqing on 16/7/31.
+//  Copyright © 2016年 liuyunqing. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
